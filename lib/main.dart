2678b8dc8bb02305d99ad7922a9e7c8ef352c17e
louiss0 <louisshelton0@gmail.com>
2026-08-13 13:55:18 -0400

    feat(application): execute commands from the main entry point

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,14 @@
+import 'package:arg_parser/errors.dart';
 import 'package:arg_parser/executor.dart';
+import 'package:arg_parser/help_formatter.dart';
 import 'package:chalkdart/chalk.dart';
 
 void main(List<String> args) {
-  print(chalk.blue("Hello"));
+  VariadicString("foo");
+
+  try {
+    Executor("mamba", "This is the Manba CLI").execute(args);
+  } on MambaException catch (e) {
+    print(chalk.red(e.message));
+  }
 }
