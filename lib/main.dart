497d3e6b8da4e170a59c36b244022166e2824719
louiss0 <louisshelton0@gmail.com>
2026-08-19 18:04:30 -0400

    fix(cli): Add spacing to CLI description

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:arg_parser/executor.dart';
 
 Future<void> main(List<String> args) async {
-  Executor("mamba", "This is the Manba CLI").execute(args);
+  Executor("mamba", "This is the Manba CLI ").execute(args);
 }
