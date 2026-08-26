7b69e22f13a8f6b90016c61c049b8af633a56870
louiss0 <louisshelton0@gmail.com>
2026-08-26 14:06:22 -0400

    remove(mamba): remove obsolete main entrypoint

--- lib/main.dart
+++
@@ -1,7 +0,0 @@
-import 'package:mamba/executor.dart';
-
-var executor = Executor("mamba", "This is the Manba CLI ").create();
-
-Future<void> main(List<String> args) async {
-  executor.execute(args);
-}
