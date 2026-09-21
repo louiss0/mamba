109ad4332c6ea03c302b1ad7057a8c157cec3bb9
louiss0 <louisshelton0@gmail.com>
2026-09-20 22:21:47 -0400

    refactor(cli): format code with vertical spacing

--- lib/mamba_cli.dart
+++ lib/mamba_cli.dart
@@ -6,32 +6,44 @@ import 'package:mamba/errors.dart';
 /// Creates a small Dart package using the current typed command API.
 final class CreateProjectCommand extends Command {
   new(this._parentDirectory) : super(mandatoryPositionals: [packageName]);
+
   static final packageName = NormalPositional(
     'package-name',
     regExp: RegExp(r'[a-z][a-z0-9_]*'),
   );
+
   final Directory _parentDirectory;
+
   @override
   String get name => 'create';
+
   @override
   String get shortDescription =>
       'Create a Dart console application using Mamba.';
+
   @override
   String run(ParsedInputs inputs, List<String> args) {
     final name = inputs.valueOf(packageName);
+
     final directory = Directory('${_parentDirectory.path}/$name');
+
     if (directory.existsSync())
       throw MambaException(
         'Cannot create $name: the directory already exists.',
       );
+
     directory.createSync();
+
     Directory('${directory.path}/bin').createSync();
+
     File('${directory.path}/pubspec.yaml').writeAsStringSync(
       'name: $name\nenvironment:\n  sdk: ^3.13.2\ndependencies:\n  mamba: any\n',
     );
+
     File('${directory.path}/bin/$name.dart').writeAsStringSync(
       "import 'package:mamba/mamba.dart';\nFuture<void> main(List<String> args) => Executor('$name', 'A command-line application.', '1.0.0', []).create().execute(args);\n",
     );
+
     return 'Created Mamba command-line application in ${directory.path}.';
   }
 }
@@ -40,34 +52,48 @@ final class CreateProjectCommand extends Command {
 final class ScaffoldCommand extends Command {
   new(this._parentDirectory)
     : super(mandatoryPositionals: [commandName], flags: [group]);
+
   static final commandName = NormalPositional(
     'name',
     regExp: RegExp(r'[a-z][a-z0-9_]*'),
   );
+
   static final group = BooleanFlag(
     'group',
     description: 'Create a group command.',
   );
+
   final Directory _parentDirectory;
+
   @override
   String get name => 'command';
+
   @override
   String get shortDescription => 'Create a Mamba command.';
+
   @override
   String run(ParsedInputs inputs, List<String> args) {
     final name = inputs.valueOf(commandName);
+
     final isGroup = inputs.valueOf(group);
+
     final file = File('${_parentDirectory.path}/lib/$name.dart');
+
     if (file.existsSync())
       throw MambaException('Cannot create $name: the file already exists.');
+
     file.parent.createSync(recursive: true);
+
     final className = '${name[0].toUpperCase()}${name.substring(1)}Command';
+
     final implementation = isGroup
         ? "final class $className extends GroupCommand {\n  new() : super([]);\n  @override String get name => '$name';\n  @override String get shortDescription => 'Describe $name.';\n}\n"
         : "final class $className extends Command {\n  @override String get name => '$name';\n  @override String get shortDescription => 'Describe $name.';\n  @override String run(ParsedInputs inputs, List<String> args) => '';\n}\n";
+
     file.writeAsStringSync(
       "import 'package:mamba/mamba.dart';\n\n$implementation",
     );
+
     return 'Created command in ${file.path}.';
   }
 }
